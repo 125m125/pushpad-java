91bc72f41916c6c181e5675459aaa7283af6fc54
125m125 <fmbba@web.de>
2017-01-17 16:24:39 +0100

    add method to check if a user is subscribed

--- src/xyz/pushpad/Pushpad.java
+++ src/xyz/pushpad/Pushpad.java
@@ -1,11 +1,21 @@
 package xyz.pushpad;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.crypto.Mac;
-import java.security.SignatureException;
-import java.security.NoSuchAlgorithmException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Pushpad {
   public String authToken;
@@ -42,5 +52,55 @@ public class Pushpad {
 
   public Notification buildNotification(String title, String body, String targetUrl) {
     return new Notification(this, title, body, targetUrl);
+  }
+
+  public boolean isSubscribed(final String uid) {
+    final String endpoint = "https://pushpad.xyz/projects/" + this.projectId + "/subscriptions?uids[]=" + uid;
+    HttpsURLConnection connection = null;
+    int code;
+    String responseBody;
+    JSONArray json;
+
+    try {
+      // Create connection
+      final URL url = new URL(endpoint);
+      connection = (HttpsURLConnection) url.openConnection();
+      connection.setRequestMethod("GET");
+      connection.setRequestProperty("Authorization", "Token token=\"" + this.authToken + "\"");
+      connection.setRequestProperty("Accept", "application/json");
+      connection.setUseCaches(true);
+      connection.setDoOutput(false);
+
+      // Get Response
+      final InputStream is = connection.getInputStream();
+      final BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+      final StringBuilder response = new StringBuilder();
+      String line;
+      while ((line = rd.readLine()) != null) {
+        response.append(line);
+        response.append('\r');
+      }
+      rd.close();
+      code = connection.getResponseCode();
+      responseBody = response.toString();
+    } catch (final IOException e) {
+      return false;
+    } finally {
+      if (connection != null) {
+        connection.disconnect();
+      }
+    }
+
+    if (code != 200) {
+      return false;
+    }
+
+    try {
+      final JSONParser parser = new JSONParser();
+      json = (JSONArray) parser.parse(responseBody);
+    } catch (final ParseException e) {
+      return false;
+    }
+    return !json.isEmpty();
   }
 }
